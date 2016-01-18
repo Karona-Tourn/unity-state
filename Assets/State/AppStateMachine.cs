6d13f96d2373f2c8b2ad5fdb6e2491d1aae968bd
Karona Tourn <tournkarona@gmail.com>
2016-01-18 07:31:19 +0700

    Updated state machine script

--- Assets/State/AppStateMachine.cs
+++ Assets/State/AppStateMachine.cs
@@ -1,8 +1,14 @@
+// Microsoft name space
 using System;
-
-using UnityEngine;
 using System.Collections.Generic;
 
+// Unity name space
+using UnityEngine;
+
+/// <summary>
+/// Control state
+/// </summary>
+/// <typeparam name="StateMachineType">Type of state machine class inherited</typeparam>
 public partial class AppStateMachine<StateMachineType> : MonoBehaviour
 	where StateMachineType : AppStateMachine<StateMachineType>
 {
@@ -10,7 +16,17 @@ public partial class AppStateMachine<StateMachineType> : MonoBehaviour
 	private Dictionary<string, AppState> cachedStates = new Dictionary<string, AppState>();
 
 	private AppState currentState;
-	private Type prevState;
+	private Type previousState;
+
+	public bool IsCurrentState<StateType>() where StateType : AppState
+	{
+		return currentState is StateType;
+	}
+
+	public bool IsPreviousState<StateType>() where StateType : AppState
+	{
+		return previousState == typeof(StateType);
+	}
 
 	#region Core Method
 
@@ -61,11 +77,12 @@ public partial class AppStateMachine<StateMachineType> : MonoBehaviour
 		if (currentState != null)
 		{
 			currentState.Exit();
-			prevState = currentState.GetType();
+			previousState = currentState.GetType();
 		}
 
 		currentState = state;
 
+		// Enter current state
 		if (currentState != null)
 		{
 			currentState.Enter();
@@ -74,14 +91,29 @@ public partial class AppStateMachine<StateMachineType> : MonoBehaviour
 
 	private void PlayState(Type stateType, bool cached)
 	{
+		AppState newState = null;
+		string stateName = stateType.Name;
+
+		if (!cachedStates.TryGetValue(stateName, out newState))
+		{
+			newState = Activator.CreateInstance(stateType) as AppState;
+			newState.StateMachine = this as StateMachineType;
+
+			// Cache state
+			if (cached)
+			{
+				cachedStates.Add(stateName, newState);
+			}
+		}
+
+		PlayState(newState);
 	}
 
-	public void PlayPrevState()
+	public void PlayPreviousState()
 	{
-		if (prevState != null)
+		if (previousState != null)
 		{
-			AppState state = Activator.CreateInstance(prevState) as AppState;
-			PlayState(state);
+			PlayState(previousState, false);
 		}
 	}
 
@@ -92,32 +124,7 @@ public partial class AppStateMachine<StateMachineType> : MonoBehaviour
 	/// <param name="cached">Tell whether the state is cached or not</param>
 	public void PlayState<StateType>(bool cached = false) where StateType : AppState, new()
 	{
-		AppState newState = null;
-		string stateName = typeof(StateType).Name;
-
-		// Exit previous state
-		if (currentState != null)
-		{
-			currentState.Exit();
-			prevState = currentState.GetType();
-		}
-
-		if (!cachedStates.TryGetValue(stateName, out newState))
-		{
-			newState = new StateType();
-			newState.StateMachine = this as StateMachineType;
-
-			// Cache state
-			if (cached)
-			{
-				cachedStates.Add(stateName, newState);
-			}
-		}
-
-		currentState = newState;
-
-		// Enter current state
-		currentState.Enter();
+		PlayState(typeof(StateType), cached);
 	}
 
 	/// <summary>
